17d4b10aeb32bfa54478745711dd7257717a09f5
duxpe <pedrogeraldiaguiar@gmail.com>
2024-11-20 23:54:47 -0300

    feature/starting-point: Adding base structure

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Holy Flake!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      nixosConfigurations.duxpe = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./modules/hardware-configuration.nix
+          ./modules/system.nix
+          ./modules/home.nix
+        ];
+      };
+
+    };
+}
---
+++ modules/system/bootloader.nix
@@ -0,0 +1,40 @@
+/*Note: Everytime you change this file, please run:
+
+> sudo nixos-rebuild switch --flake .#duxpe --install-bootloader
+
+*/
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+{
+  # Grub configuration
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = false;
+  boot.loader.grub.default = "saved";
+  boot.loader.grub.memtest86.enable = false;
+  boot.loader.grub.fontSize = 26;
+  boot.loader.grub.gfxmodeEfi = "auto";
+  boot.loader.grub.gfxmodeBios = "auto";
+  boot.loader.grub.extraEntries = ''
+    submenu "More Options..." {
+      menuentry "Reboot" {
+        reboot
+      }
+      menuentry "Poweroff" {
+        halt
+      }
+      menuentry "UEFI Firmware Settings" {
+        fwsetup
+      }
+    }
+  '';
+
+}
